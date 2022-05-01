14cf1ae84b1ddcb6654b94b5251da30e377c13fa
IsaacOus <iouslimane@myges.fr>
2022-05-01 15:01:39 +0200

    initial commit

---
+++ src/main/java/com/esgi/jeeproject/vibecritical/VibeCriticalApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.jeeproject.vibecritical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeCriticalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibeCriticalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esgi/jeeproject/vibecritical/VibeCriticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.jeeproject.vibecritical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeCriticalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
