46eb9a4c049b110475e9aadb12ea4ed9b52ac84d
IsaacOus <iouslimane@myges.fr>
2022-05-30 23:04:31 +0200

    [ADD] Spring Security Configuration -> Authentication Attempt

--- src/main/java/com/esgi/jeeproject/vibecritical/service/UserServiceImpl.java
+++ src/main/java/com/esgi/jeeproject/vibecritical/service/UserServiceImpl.java
@@ -4,17 +4,37 @@ import com.esgi.jeeproject.vibecritical.domain.Role;
 import com.esgi.jeeproject.vibecritical.domain.User;
 import com.esgi.jeeproject.vibecritical.repositories.RoleRepository;
 import com.esgi.jeeproject.vibecritical.repositories.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found in the databse");
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(username, user.getPassword(),authorities);
+    }
 
     public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
