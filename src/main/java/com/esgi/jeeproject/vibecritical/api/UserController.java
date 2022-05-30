996c671edc66a4d8b4230c98c8a66429f06312fe
IsaacOus <iouslimane@myges.fr>
2022-05-30 22:22:56 +0200

    [ADD] End points + Spring Security

--- src/main/java/com/esgi/jeeproject/vibecritical/api/UserController.java
+++ src/main/java/com/esgi/jeeproject/vibecritical/api/UserController.java
@@ -1,20 +1,19 @@
 package com.esgi.jeeproject.vibecritical.api;
 
+import com.esgi.jeeproject.vibecritical.domain.Role;
 import com.esgi.jeeproject.vibecritical.domain.User;
 import com.esgi.jeeproject.vibecritical.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
     private final UserService userService;
-
-
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -25,4 +24,20 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody  RoleToUserForm form){
+        userService.addRoleToUser(form.getUsername(), form.getRolename());
+        return ResponseEntity.ok().build();
+    }
 }
