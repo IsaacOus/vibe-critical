6fe4c78c7940818d693d0ec398dde643f8faaa7c
ahmad2743 <amethdione10@gmail.com>
2022-06-01 21:36:42 +0200

    update ci/cd file and application properties

--- src/main/java/com/esgi/jeeproject/vibecritical/VibeCriticalApplication.java
+++ src/main/java/com/esgi/jeeproject/vibecritical/VibeCriticalApplication.java
@@ -3,11 +3,14 @@ package com.esgi.jeeproject.vibecritical;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class VibeCriticalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VibeCriticalApplication.class, args);
+        System.out.println("it work");
     }
 
 }
